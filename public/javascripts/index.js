8e4ea4b3c13824d3fcd6e8d6b1c58d09788d9de8
jeff <jeffxenog@qq.com>
2013-07-24 16:12:32 +0800

    修改代码位置

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -34,97 +34,112 @@ var w_expend= { //功勋对应猫粮消耗
         '26':620
 };
 
-$('.J_army').each(function(){       //当前武将猫粮消耗及255功勋武将标红
-    //console.log(111);
-    var str='';
-    var _arr=$(this).text().split(',');
-    var feats=0;                          //当前武将消耗
-    var expend=0;                         //当前武将们消耗
-    _arr.forEach(function(value,index){
-        feats=parseFloat(value);
-        //为220-225以及255功勋的武将做特殊判断
-        if( feats>220&&feats<=225){
-            feats=530
-        } else{
-            if(feats==255){
-                feats=620
-            }else{
-                feats = w_expend[Math.ceil((feats/10))];  //根据w_expend对象计算武将出征消耗
-            }
-        }
-        expend +=feats;  //各武将值消耗值相加
-    })
 
-    expend=expend/5; //出征消耗
-      _arr.forEach(function(value,index){          //255功勋武将标红
-           if(value==='255'){
-            str+='<span style="color: #F00">'+value+'</span>';
-        }else{
-            str+=value;
-        }
-        str+=',';
-    });
-    str=str.substr(0, str.length-1);
-    str=str+'&nbsp;&nbsp;&nbsp;('+expend+")";
-    $(this).html(str);
-});
 
-$('.J_build').each(function(){  //设置各个建筑颜色
-    var str='';
-    var _arr=$(this).text().split(',');
-    _arr.forEach(function(value,index){
-        var buildType=value.substr(0,value.length-5);
-        var buildLV=value.substr(value.length-5,value.length);
-        switch (buildType){
-            case '水田':
-                str+='<b style="color: green">'+buildType+buildLV+'</b>';
-                break;
-            case '兵粮库':
-                str+='<b style="color: #DDAA00">'+buildType+buildLV+'</b>';
-                break;
-            case '宝物库':
-                str+='<b style="color: dodgerblue">'+buildType+buildLV+'</b>';
-                break;
-            case '公馆':
-                str+='<b style="color: #CC0000">'+buildType+buildLV+'</b>';
-                break;
-            default:
-                str+=buildType+buildLV;
-                break;
-        }
-        str+=',';
-    });
-    str=str.substr(0, str.length-1);
-    $(this).html(str);
-});
-
-$('.J_resources').each(function(){  //设置各个资源颜色
-    var str='';
-    var _arr=$(this).text().split(',');
-    str+='<b style="color: #CC0000">'+_arr[0]+'</b>,';
-    str+='<b style="color: green">'+_arr[1]+'</b>,';
-    str+='<b style="color: #DDAA00">'+_arr[2]+'</b>,';
-    str+='<b style="color: dodgerblue">'+_arr[3]+'</b>,';
-    str+='<b style="color: #696969">'+_arr[3]+'</b>,';
-    str=str.substr(0, str.length-1);
-    $(this).html(str);
-});
-
-$('.postDel').on('click',function(){  //删除账号
-    if (!confirm("确认要删除？")) {
-        window.event.returnValue = false;
-    }else{
-        var id=$(this).val();
-        console.log(id);
-        $.post('/delete',{id:id},function(){
+(function($){
+    $(function(){
+        //绑事件
+        $("#hide").on('click',function(){
+            $(".other").hide();
+            $(".main").show();
+        });
+        $("#show").on('click',function(){
+            $(".other").show();
+            $(".main").hide();
+        });
+        $("#reload").on('click',function(){
             window.location.reload(true);
         });
-    }
-});
 
-(function($){  //计算所有账号总喵喵点
-    $(function(){
-        var totalMoney=(function(){
+        $('.J_army').each(function(){       //当前武将猫粮消耗及255功勋武将标红
+            //console.log(111);
+            var str='';
+            var _arr=$(this).text().split(',');
+            var feats=0;                          //当前武将消耗
+            var expend=0;                         //当前武将们消耗
+            _arr.forEach(function(value,index){
+                feats=parseFloat(value);
+                //为220-225以及255功勋的武将做特殊判断
+                if( feats>220&&feats<=225){
+                    feats=530
+                } else{
+                    if(feats==255){
+                        feats=620
+                    }else{
+                        feats = w_expend[Math.ceil((feats/10))];  //根据w_expend对象计算武将出征消耗
+                    }
+                }
+                expend +=feats;  //各武将值消耗值相加
+            })
+
+            expend=expend/5; //出征消耗
+            _arr.forEach(function(value,index){          //255功勋武将标红
+                if(value==='255'){
+                    str+='<span style="color: #F00">'+value+'</span>';
+                }else{
+                    str+=value;
+                }
+                str+=',';
+            });
+            str=str.substr(0, str.length-1);
+            str=str+'&nbsp;&nbsp;&nbsp;('+expend+")";
+            $(this).html(str);
+        });
+
+        $('.J_build').each(function(){  //设置各个建筑颜色
+            var str='';
+            var _arr=$(this).text().split(',');
+            _arr.forEach(function(value,index){
+                var buildType=value.substr(0,value.length-5);
+                var buildLV=value.substr(value.length-5,value.length);
+                switch (buildType){
+                    case '水田':
+                        str+='<b style="color: green">'+buildType+buildLV+'</b>';
+                        break;
+                    case '兵粮库':
+                        str+='<b style="color: #DDAA00">'+buildType+buildLV+'</b>';
+                        break;
+                    case '宝物库':
+                        str+='<b style="color: dodgerblue">'+buildType+buildLV+'</b>';
+                        break;
+                    case '公馆':
+                        str+='<b style="color: #CC0000">'+buildType+buildLV+'</b>';
+                        break;
+                    default:
+                        str+=buildType+buildLV;
+                        break;
+                }
+                str+=',';
+            });
+            str=str.substr(0, str.length-1);
+            $(this).html(str);
+        });
+
+        $('.J_resources').each(function(){  //设置各个资源颜色
+            var str='';
+            var _arr=$(this).text().split(',');
+            str+='<b style="color: #CC0000">'+_arr[0]+'</b>,';
+            str+='<b style="color: green">'+_arr[1]+'</b>,';
+            str+='<b style="color: #DDAA00">'+_arr[2]+'</b>,';
+            str+='<b style="color: dodgerblue">'+_arr[3]+'</b>,';
+            str+='<b style="color: #696969">'+_arr[3]+'</b>,';
+            str=str.substr(0, str.length-1);
+            $(this).html(str);
+        });
+
+        $('.postDel').on('click',function(){  //删除账号
+            if (!confirm("确认要删除？")) {
+                window.event.returnValue = false;
+            }else{
+                var id=$(this).val();
+                console.log(id);
+                $.post('/delete',{id:id},function(){
+                    window.location.reload(true);
+                });
+            }
+        });
+
+        var totalMoney=(function(){//计算所有账号总喵喵点
             var money=0;
             $('#accountListTb').find('tbody').find('tr').each(function(){
                 money+=parseInt($(this).find('td[data-field="money"]').text(),10);
